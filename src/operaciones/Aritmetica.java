69c7f8a978cbd7b63b968f77eccb29415f8bf7aa
Rick <Rick@DESKTOP-N10MD3R>
2022-03-08 15:04:41 -0600

    50. Creación de métodos en Java (Creación del método suma)

--- src/operaciones/Aritmetica.java
+++ src/operaciones/Aritmetica.java
@@ -1,5 +1,14 @@
 package operaciones;
 
 public class Aritmetica {
-    
+//Primero se ponen los atributos y despues los métodos    
+//Atributos de la clase
+    int a;
+    int b;
+    //metodo
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("Resultado:"+resultado);
+         
+    }
 }
